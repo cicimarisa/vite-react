3fa57bd51e898212937d4b5d5b5c34d7acc2bae0
Cici Marisa Hardianti <cicimarisahardianti@gmail.com>
2024-09-06 17:44:06 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        id: 1,
+        title: "Eiffel Tower",
+        location: "France",
+        googleMapsUrl: "https://maps.app.goo.gl/kP1Nw9LZorS3QDtT9",
+        startDate: "05 Mar, 2023",
+        endDate: "12 Mar, 2023",
+        description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It was named after the engineer Gustave Eiffel, whose company designed and built the tower.",
+        imageUrl: "https://images.unsplash.com/photo-1502588763795-b8dbc5fb7e67?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 2,
+        title: "Great Wall of China",
+        location: "China",
+        googleMapsUrl: "https://maps.app.goo.gl/5px6TocLdTTXM1hh8",
+        startDate: "15 Jun, 2022",
+        endDate: "22 Jun, 2022",
+        description: "The Great Wall of China is a series of fortifications made of various materials to protect the northern borders of the Chinese Empire from invasions. It is one of the greatest architectural feats in history.",
+        imageUrl: "https://images.unsplash.com/photo-1713346643797-34e3e7188833?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 3,
+        title: "Machu Picchu",
+        location: "Peru",
+        googleMapsUrl: "https://maps.app.goo.gl/97zvBp6iMTK1tvhDA",
+        startDate: "01 Sep, 2024",
+        endDate: "10 Sep, 2024",
+        description: "Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru on a 2,430-meter (7,970 ft) peak. It’s known for its dry-stone walls and panoramic views of the surrounding mountains.",
+        imageUrl: "https://images.unsplash.com/photo-1719015457432-1ba4968dfde4?q=80&w=2828&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 4,
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://maps.app.goo.gl/wv9tBYSqYjpEhz9C8",
+        startDate: "18 Oct, 2023",
+        endDate: "25 Oct, 2023",
+        description: "The Sydney Opera House is a multi-venue performing arts centre at Sydney Harbour. It is known for its unique design and is one of the most photographed landmarks in the world.",
+        imageUrl: "https://plus.unsplash.com/premium_photo-1697730221799-f2aa87ab2c5d?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 5,
+        title: "Taj Mahal",
+        location: "India",
+        googleMapsUrl: "https://maps.app.goo.gl/ukEGU8DV9h23g4LZ7",
+        startDate: "20 Apr, 2023",
+        endDate: "30 Apr, 2023",
+        description: "The Taj Mahal is an Islamic ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, India. It was commissioned in 1632 by Mughal Emperor Shah Jahan to house the tomb of his favorite wife, Mumtaz Mahal.",
+        imageUrl: "https://images.unsplash.com/photo-1523980077198-60824a7b2148?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 6,
+        title: "Amsterdam Canals",
+        location: "Amsterdam, Netherlands",
+        googleMapsUrl: "https://maps.app.goo.gl/gsEsjvRLYoS2UVXt9",
+        startDate: "01 Apr, 2024",
+        endDate: "07 Apr, 2024",
+        description: "The Amsterdam Canals are a system of concentric canals and waterways in Amsterdam, known for their unique architecture and picturesque views.",
+        imageUrl: "https://images.unsplash.com/photo-1706043634913-12e36557c3c3?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 7,
+        title: "Rotterdam Skyline",
+        location: "Rotterdam, Netherlands",
+        googleMapsUrl: "https://maps.app.goo.gl/shZjochbqQXHqB71A",
+        startDate: "08 Apr, 2024",
+        endDate: "14 Apr, 2024",
+        description: "Rotterdam is known for its modern architecture and impressive skyline, including the iconic Erasmus Bridge and the Cube Houses.",
+        imageUrl: "https://images.unsplash.com/photo-1658430615341-a1c075ad4233?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fFJvdHRlcmRhbSUyMFNreWxpbmV8ZW58MHx8MHx8fDA%3D"
+    },
+    {
+        id: 9,
+        title: "Gold Coast Beaches",
+        location: "Gold Coast, Australia",
+        googleMapsUrl: "https://maps.app.goo.gl/r3YTV8hAtfZwpDF76",
+        startDate: "22 Apr, 2024",
+        endDate: "28 Apr, 2024",
+        description: "The Gold Coast is famous for its stunning beaches, surf culture, and vibrant nightlife.",
+        imageUrl: "https://images.unsplash.com/photo-1686007579079-550a74a0decb?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
